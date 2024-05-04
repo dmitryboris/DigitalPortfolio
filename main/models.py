9df1960ec379b60e1809e28beabb059deac3350c
Dima <dmitrij_bo05@mail.ru>
2024-05-04 16:57:58 +0500

    create model Achievements

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Achievements(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField()
+    user_id = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date of publication')
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Achievement'
+        verbose_name_plural = 'Achievements'
