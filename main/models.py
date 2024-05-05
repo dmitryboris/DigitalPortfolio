a12c4468b7170c498609139f421c46524e434d5a
Dima <dmitrij_bo05@mail.ru>
2024-05-05 13:38:45 +0500

    update model achievements

--- main/models.py
+++ main/models.py
@@ -1,13 +1,19 @@
+from django.conf import settings
 from django.db import models
 
 
 class Achievements(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField()
-    user_id = models.IntegerField(default=0)
-    pub_date = models.DateTimeField('date of publication')
+    pub_date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
     def __str__(self):
         return self.title
