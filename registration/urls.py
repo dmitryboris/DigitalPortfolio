2b023764561c4dfb5f1522b72dd0124e954b7c00
Dima <dmitrij_bo05@mail.ru>
2024-05-06 20:04:15 +0500

    add db

--- registration/urls.py
+++ registration/urls.py
@@ -2,8 +2,6 @@ from .views import register, login, profile_view, password_reset, RegisterView
 from django.urls import path
 
 urlpatterns = [
-    # path('register/', register, name='register'),
-    # path('login/', login, name='login'),
     # path('password_reset/', password_reset, name='password_reset'),
     path('profile/', profile_view, name="profile"),
     path('register/', RegisterView.as_view(), name="register"),
