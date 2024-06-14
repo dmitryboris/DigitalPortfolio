88b2dbf9fafe6168843c850084e5a092fb6a6518
Dima <dmitrij_bo05@mail.ru>
2024-06-14 17:19:13 +0500

    found a bug: only owner can like/dislike

--- main/templatetags/custom_filters.py
+++ main/templatetags/custom_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='file_ext')
 def file_ext(url, ext):
     return url.lower().endswith(ext)
+
+
+@register.filter(name='liked')
+def file_ext(user, achievement):
+    return user in achievement.liked_by.all()
